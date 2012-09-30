b8ad7143a4733a205bcb946a59aea907d6d960c9
haydnnewport <haydnnewport@gmail.com>
2012-09-30 15:14:35 +1300

    Stuff

--- SWEN303Project/src/canvas/ScatterCanvas.java
+++ SWEN303Project/src/canvas/ScatterCanvas.java
@@ -1,9 +1,70 @@
 package canvas;
 
+import java.awt.Dimension;
+import java.awt.Graphics;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ScatterCanvas {
-	List<Point> points = new ArrayList<Point>();
-	
+import javax.swing.JPanel;
+
+public class ScatterCanvas extends JPanel {
+	// The point list
+	List<StatPoint> points = new ArrayList<StatPoint>();
+	// Extra drawable items
+	List<Drawable> drawables = new ArrayList<Drawable>();
+	double xMaxV = -Double.MAX_VALUE;
+	double yMaxV = -Double.MAX_VALUE;
+	double xMinV = 0;
+	double yMinV = 0;
+	Dimension scaleFactor;
+	Dimension offset;
+
+	Dimension size = new Dimension(800, 600);
+
+	public ScatterCanvas() {
+		setPreferredSize(size);
+		setMaximumSize(size);
+		setMinimumSize(size);
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		// Paint the axes
+		calculateAxes();
+		if (xMaxV == xMinV || yMaxV == yMinV) {
+			return;
+		}
+
+		for (StatPoint p : points) {
+			p.calculate(scaleFactor, offset);
+			p.drawSelf(g);
+		}
+		for (Drawable d : drawables) {
+			d.drawSelf(g);
+		}
+	}
+
+	public Dimension valueToCoord(Dimension val) {
+
+		return new Dimension(val);
+	}
+
+	private void calculateAxes() {
+		for (StatPoint p : points) {
+			double x = p.getXVal();
+			double y = p.getYVal();
+			xMaxV = (x > xMaxV) ? x : xMaxV;
+			yMaxV = (y > yMaxV) ? y : yMaxV;
+			xMinV = (x < xMinV) ? x : xMinV;
+			yMinV = (y < yMinV) ? y : yMinV;
+		}
+
+		scaleFactor = new Dimension((int) Math.round(getSize().getWidth()
+				/ (xMaxV - xMinV)), (int) Math.round(getSize().getHeight()
+				/ (yMaxV - yMinV)));
+		offset = new Dimension((int) (scaleFactor.getWidth() * xMinV),
+				(int) (scaleFactor.getHeight() * yMinV));
+	}
+
 }
--- SWEN303Project/src/canvas/StatPoint.java
+++ SWEN303Project/src/canvas/StatPoint.java
@@ -1,24 +1,87 @@
 package canvas;
 
+import java.awt.Dimension;
 import java.awt.Graphics;
 
 public class StatPoint extends Point implements Drawable {
 	// A stat point is a drawable object that has a position, a label and when
 	// necessary a movement vector
 	public static final int POINT_SIZE = 4;
-	
+
 	private Vector dest;
 	private String label;
+	private double xV, yV;
+	private boolean enabled = false;
 
-	public StatPoint(int x, int y) {
-		super(x, y);
+//	public StatPoint(int x, int y) {
+//		super(x, y);
+//		dest = null;
+//		label = null;
+//		enabled = true;
+//	}
+	
+	public StatPoint(StatPoint old){
+		super(old.x, old.y);
+		dest = old.dest;
+		label = old.label;
+		xV = old.xV;
+		yV = old.yV;
+		enabled = old.enabled;
+	}
+	
+	public StatPoint(StatPoint old, String label){
+		super(old.x, old.y);
+		dest = old.dest;
+		this.label = label;
+		xV = old.xV;
+		yV = old.yV;
+		enabled = old.enabled;
+	}
+
+	public StatPoint(double xVal, double yVal) {
+		super(0, 0);
+		xV = xVal;
+		yV = yVal;
 		dest = null;
 		label = null;
 	}
 
+	public void calculate(Dimension scaleFactor, Dimension offset) {
+		setX((int) (Math.round(xV * scaleFactor.getWidth()) + offset.getWidth()));
+		setY((int) (Math.round(yV * scaleFactor.getHeight()) + offset.getHeight()));
+		enabled = true;
+	}
+
 	@Override
 	public boolean drawSelf(Graphics g) {
-		g.drawOval(x-POINT_SIZE/2, y-POINT_SIZE/2, POINT_SIZE, POINT_SIZE);
-		return true;
+		if (enabled)
+			g.drawOval(x - POINT_SIZE / 2, y - POINT_SIZE / 2, POINT_SIZE,
+					POINT_SIZE);
+		return enabled;
 	}
+
+	public Vector getDest() {
+		return dest;
+	}
+
+	public void setDest(Vector dest) {
+		this.dest = dest;
+	}
+
+	public double getXVal() {
+		return xV;
+	}
+
+	public void setXVal(double xV) {
+		this.xV = xV;
+	}
+
+	public double getYVal() {
+		return yV;
+	}
+
+	public void setYVal(double yV) {
+		this.yV = yV;
+	}
+	
 }
